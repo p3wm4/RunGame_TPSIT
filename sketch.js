6197b629ede662703b9882bbf720de7ad85f1f97
⋆˚࿔ pew 𝜗𝜚˚⋆ <172263940+p3wm4@users.noreply.github.com>
2026-09-02 17:55:18 +0200

    Update sketch.js
    Seconda versione del RunGame, aggiornata in data 31/06/2026. In questa versione è stato aggiunto il pulsante "START", che segnala l'inizio del gioco e della gara. Inoltre, i contendenti sono stati aggiornati con colori diversi ed è stata modificata la loro funzione di movimento, in modo che inizino a muoversi solamente dopo aver premuto il pulsante.

--- sketch.js
+++ sketch.js
@@ -2,42 +2,66 @@ let startLine = 30;
 let finishLine = 430;
 
 let spacing = 20;
-let segmentSize = 50;
-
 let numContenders = 4;
+
+let circX = startLine;
+let colors = [
+  [255, 105, 180],     // Pink
+  [128, 0, 128],     // Purple
+  [255, 0, 0],   // Red
+  [0, 128, 128]    // Teal
+];
+
+let raceStarted = false;
 
 function setup() {
   createCanvas(500, 500);
   frameRate(3);
+
+  let startButton = createButton("START");
+  startButton.position(220, 520);
+  startButton.mousePressed(startRace);
 }
 
 function draw() {
-  background(89, 70, 100);
-  
+  background(128, 128, 128);
+
   noStroke();
   fill(0);
   rect(startLine, 0, 5, height);
-  fill(0, 0, 255);
-  rect(finishLine, 0, 20, height);
-  drawContender(circX, circY-150);
-  drawContender(circX, circY);
-  drawContender(circX, circY+150);
 
-  circX += spacing;
+  fill(0, 255, 0);
+  rect(finishLine, 0, 20, height);
+
+  if (raceStarted) {
+    circX += spacing;
+  }
+
+  drawContenders();
+
   if (circX > finishLine) {
-    noLoop();
+    raceStarted = false;
   }
 }
 
-function drawContender() {
-  let x = circX;
+function drawContenders() {
+  let padding = height / numContenders;
 
-  for (let i = 0; i < segments; i += 1) {
-    fill(255, 0, 200);
-    stroke(0);
-    strokeWeight(1);
-    circle(x, y, 50);
+  for (let i = 0; i < numContenders; i += 1) {
+    let y = (i + 0.5) * padding;
 
-    x += spacing;
+    drawContender(circX, y, colors[i]);
   }
+}
+
+function drawContender(x, y, color) {
+  fill(color);
+  stroke(0);
+  strokeWeight(1);
+
+  circle(x, y, 50);
+}
+
+function startRace() {
+  raceStarted = true;
 }
